17bebed6cb5fcf7065daddfcd2855e44d7401df3
celanajaya <petermsteele@gmail.com>
2017-07-26 07:30:39 -0500

    Initial Commit

---
+++ AdjustableTextField/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdjustableTextField
+//
+//  Created by Peter Steele on 7/26/17.
+//  Copyright © 2017 peteyP. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AdjustableTextField/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AdjustableTextField
+//
+//  Created by Peter Steele on 7/26/17.
+//  Copyright © 2017 peteyP. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
